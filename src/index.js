a56c99448f11d0946ad3e07759323ab431387aca
Elena Surilova <elen21surilova@gmail.com>
2021-03-11 01:05:50 +0200

    feat: add solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	for (let i = 0; i < bracketsConfig.length; i++) {
+		let brackStr = bracketsConfig[i].join('');
+		if (str.includes(brackStr)) {
+			str = str.replace(brackStr, '');
+			i = -1;
+		}
+	}
+	str = str.trim();
+	if (str.length <= 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
